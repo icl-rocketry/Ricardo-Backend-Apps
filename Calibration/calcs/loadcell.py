fa78e91c2f6b85c84c1e88d91af12a7aa03565e1
Andrei <paduraru.andrei5@gmail.com>
2025-05-13 15:10:41 +0100

    Forgot a 1e-3 and a g in the loadcell calcs

--- Calibration/calcs/loadcell.py
+++ Calibration/calcs/loadcell.py
@@ -1,13 +1,15 @@
 #Sensor properties
 mvperV = 2.0 #Output in mV at full scale range per input Volt (from datasheet)
 FSR = 100 #Full scale range in kg
-supplyV = 12
+supplyV = 5
 
 #ADC properties
 adcMaxCounts = 2**23 - 1
 adcMaxV = 1.2
 adcGain = 1
 
-m = adcMaxCounts * (mvperV * supplyV / (adcMaxV * 1/adcGain))/FSR
+g = 9.80655
+
+m = adcMaxCounts * (mvperV * 1e-3 * supplyV / (adcMaxV * 1/adcGain))/ (FSR * g)
 
 print("Gradient: " + str(m))
